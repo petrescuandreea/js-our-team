1aa0049cd27bdff9006c0df4b17f2505150a35a4
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-04 15:24:56 +0100

    macro-step-logici

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+
+TRACCIA: Ricreiamo le card del nostro team aggiungendo al layout di base fornito, il nostro javascript in cui:
+- Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. (nel nostro file js)
+Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+- Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.
+- Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
+
+
+*/
+
+
+// 1. creo un array di oggetti
+// 2. stampo le card in pagina => for
+// 3. permetto all'utente di aggiungere nuovi membri 
